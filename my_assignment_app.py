55e9215cd0241241e37734eeb32cfe9f563cc2c2
saleemhassanvazir <saleemhassan161@gmail.com>
2024-11-26 22:08:20 +0500

    Update my_assignment_app.py

--- my_assignment_app.py
+++ my_assignment_app.py
@@ -3,9 +3,9 @@ import pandas as pd
 from joblib import load
 import dill
 
-with open('pipeline_Assignment.pkl', 'rb') as file:
+with open("C:\Users\Lenovo\Downloads\pipeline_Assignment.pkl", 'rb') as file:
     model = dill.load(file)
-my_feature_dict = load(r'my_feature_dict_Assignment.pkl')
+my_feature_dict = load("C:\Users\Lenovo\Downloads\my_feature_dict_Assignment.pkl")
 
 st.header("MLOPS assignment")
 st.subheader("Created by: Saleem Hassan Vazir")
